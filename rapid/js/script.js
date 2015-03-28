f1436c6f8642831040b2032c1a43a9002d7b144c
codecumb <tutozen.helpline@gmail.com>
2015-03-28 23:31:09 +0600

    Pre Loader Added
    Added custom preloader using fakeLoader.js in rapid.

--- rapid/js/script.js
+++ rapid/js/script.js
@@ -7,9 +7,22 @@ $(document).foundation({
 });
 
 $(document).ready(function(){
+
+    // bx-slider
+
   	$('.bxslider').bxSlider({
   		auto: true,
   		randomStart: true,
   		autoHover: true,
   	});
+
+    // fakeLoader.js
+
+    $("#fakeLoader").fakeLoader({
+      timeToHide:7000, //Time in milliseconds for fakeLoader disappear
+      zIndex:"999",//Default zIndex
+      spinner:"spinner6",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+      bgColor:"#44C0C2", //Hex, RGB or RGBA colors
+      // imagePath:"yourPath/customizedImage.gif" //If you want can you insert your custom image
+    });
 });
